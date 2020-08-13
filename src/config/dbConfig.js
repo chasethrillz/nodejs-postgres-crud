1623f98a59c09880cd356fdba50ba4a6e3c7c8e9
Vishal Kumar <vishal.epitome@gmail.com>
2020-08-13 09:52:54 +0530

    Create, Read, Delete on Users

---
+++ src/config/dbConfig.js
@@ -0,0 +1,14 @@
+/**
+ * dbConfig.js
+ * Vishal Kumar
+ */
+
+'use strict';
+
+module.exports = {
+    username: `postgres`,
+    password: `0000`,
+    host: '127.0.0.1',
+    port: '5432',
+    db: Pack.name
+};
